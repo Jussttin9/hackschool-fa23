7202faa2258b725773ca624821984040ba3dd90b
jtran-9 <jrtran@ucsd.edu>
2024-05-07 11:18:51 -0700

    created React components

--- client/src/components/game-history-component/gameHistory.jsx
+++ client/src/components/game-history-component/gameHistory.jsx
@@ -5,3 +5,16 @@ import styles from './gameHistory.module.css';
 // It should take in props for the sentence, correctChars, incorrectChars, wpm, and time
 // Export your GameHistory component!
 
+const gameHistoryCard = (props) => {
+    return (
+        <div className={styles.card}>
+            <p><b>Sentence:</b> {props.sentence}</p>
+            <p><b>Correct Characters:</b> {props.correct}</p>
+            <p><b>Incorrect Characters:</b> {props.incorrect}</p>
+            <p><b>Words per Minute:</b> {props.wpm}</p>
+            <p><b>Time (in min):</b> {props.time}</p>
+        </div>
+    )
+};
+
+export default gameHistoryCard;
--- client/src/pages/gameHistory.jsx
+++ client/src/pages/gameHistory.jsx
@@ -1,18 +1,19 @@
 // Import the GameHistory component
-
+import GameHistoryCard from "@/components/game-history-component/gameHistory";
+import styles from '../components/game-history-component/gameHistory.module.css';
 
 
 export default function GameHistory() {
     return (
         <div className={styles.container}>
             <h1>Previous Games</h1>
-
-            {/* Container for top 3 games */}
-
-
-
-
-            {/* Container for all games */}
+            <div className={styles.cardContainer}>
+                {/* Container for top 3 games */}
+                <GameHistoryCard sentence="boo, I scared you there didn't I?" correct="42" incorrect="8" wpm="42" time="1.00"/>
+                <GameHistoryCard sentence="boo, I scared you there didn't I?" correct="42" incorrect="8" wpm="42" time="1.00"/>
+                <GameHistoryCard sentence="boo, I scared you there didn't I?" correct="42" incorrect="8" wpm="42" time="1.00"/>
+                {/* Container for all games */}
+            </div>
         </div>
     );
 }
